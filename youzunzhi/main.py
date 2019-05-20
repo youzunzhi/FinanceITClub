a12e8c00ef76cfb228d95c101ae9ad48aac4d642
mac <mac@dazhi.local>
2019-05-21 00:17:20 +0800

    message['To'] must be valid

--- youzunzhi/main.py
+++ youzunzhi/main.py
@@ -7,18 +7,20 @@ import time
 def SendMail(username, password, to_list, content, subject):
     mail_host = "smtp.163.com"
     message = MIMEText(content, 'plain', 'utf-8')
-    message['From'] = Header('Python')
-    message['To'] = Header(';'.join(to_list))
+    message['From'] = username
     message['Subject'] = Header(subject, 'utf-8')
     try:
         smtp_obj = smtplib.SMTP()
+        # smtp_obj.set_debuglevel(1)
         smtp_obj.connect(mail_host, 25)
         smtp_obj.login(username, password)
         print('Login Success!')
         for to_addr in to_list:
+            message['To'] = to_addr
             smtp_obj.sendmail(username, to_addr, message.as_string())
         print('Send Email Success!')
         smtp_obj.quit()
+        print('Done.')
     except smtplib.SMTPException:
         print('SMTPException')
 
@@ -33,13 +35,14 @@ def SendTimingMail(send_hour, send_minute, username, password, to_list, content,
 
 if __name__ == '__main__':
     username = "youzunzhi@163.com"
-    password = "xxx"
-    to_list = ["youzunzhi@163.com", "youzunzhi@163.com"]
+    password = input("Input Password: ")
+    to_list = ["youzzh@mail2.sysu.edu.cn", "454367695@qq.com"]
     content = "Hi there."
     subject = 'Python Mail Test'
 
-    send_hour = 21
-    send_minute = 8
+    send_hour = 0
+    send_minute = 14
 
+    # SendMail(username, password, to_list, content, subject)
     SendTimingMail(send_hour, send_minute, username, password, to_list, content, subject)
-    print('Done.')
+
